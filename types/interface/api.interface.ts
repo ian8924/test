ed27541a93accfb3a904aef307dc14b7313f14d8
Ian Liao <ianliao0915@gmail.com>
2026-03-03 16:01:25 +0800

    remove

--- types/interface/api.interface.ts
+++ types/interface/api.interface.ts
@@ -1,32 +1,3 @@
-export interface ApiRequestPagination {
-  page?: number
-  size?: number
-  sort?: string
-  order?: 'asc' | 'desc'
-}
-
-export interface ApiResponseList<T> {
-  content: T
-  empty: boolean
-  first: boolean
-  last: boolean
-  number: number
-  numberOfElements: number
-  pageable: {
-    pageNumber: number
-    pageSize: number
-    sort: {
-      sorted: boolean
-      empty: boolean
-      unsorted: boolean
-    }
-    offset: number
-  }
-  size: number
-  totalElements: number
-  totalPages: number
-}
-
 export interface ApiResponseSingle<T> {
   data: T
   success: boolean
